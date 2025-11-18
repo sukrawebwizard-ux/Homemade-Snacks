646ef3022806d08d8a7195cde929d1426514c899
vvsBiz <164856762+vvsBiz@users.noreply.github.com>
2025-11-18 16:15:32 +0100

    subscribe your plan

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/-Homemade-Snacks/',
+  base: '/',
   build: {
     // GitHub Pages can only deploy from root or /docs
     outDir: 'docs',
