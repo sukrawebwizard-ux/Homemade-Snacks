4eac72685f4362dee9c32fb21ebb1351a21cd814
vvsBiz <164856762+vvsBiz@users.noreply.github.com>
2025-11-18 17:41:17 +0100

    subscription build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/snacksinmalta/',       // URL prefix
+  base: '/',       // URL prefix
   build: {
     outDir: 'docs',              // Must output files here
     emptyOutDir: true,
