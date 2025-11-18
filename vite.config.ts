6329b0eb309de9e425ed3db335c95cf0af4d3591
vvsBiz <164856762+vvsBiz@users.noreply.github.com>
2025-11-18 17:19:51 +0100

    changed the config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/snacksinmalta/',
+  base: '/snacksinmalta/',       // URL prefix
   build: {
-    // GitHub Pages can only deploy from root or /docs
-    outDir: 'docs',
+    outDir: 'docs',              // Must output files here
+    emptyOutDir: true,
   },
 });
