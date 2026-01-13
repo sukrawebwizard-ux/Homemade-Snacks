ba4cf9d7509d1adf33d8b1f1f02b9c95c817c301
vvsBiz <164856762+vvsBiz@users.noreply.github.com>
2026-01-13 14:53:51 +0100

    checking the vercel issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/Homemade-Snacks/'
-    : '/',
-
+  base: '/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
-
   plugins: [react()],
 })
+
